2b63f29e649713b3acaf02083c2e22f3206738a6
Tomasz Statkowski <Tomasz.Statkowski@chrobinson.com>
2021-07-28 19:09:41 +0200

    Add changes

--- TestAutomationLibrary.Selenium/Browser.cs
+++ TestAutomationLibrary.Selenium/Browser.cs
@@ -1,13 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace TestAutomationLibrary.Selenium
 {
     public class Browser
     {
+        public Browser() { }
 
+        public static IWebDriver driver;
+
+        public static void Init()
+        {
+            driver = new ChromeDriver(@"C:\Users\stattom\source\repos\TestAutomationLibrary\TestAutomationLibrary.Selenium\Resources\");
+        }
+
+        public static void Cleanup()
+        {
+            driver.Quit();
+        }
     }
 }
---
+++ TestAutomationLibrary.Tests/FakeStore/TC_Login.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using TestAutomationLibrary.Selenium;
+
+namespace TestAutomationLibrary.Tests.FakeStore
+{
+    [TestFixture]
+    public class TC_Login
+    {
+        [SetUp]
+        public void Init()
+        {
+            Browser.Init();
+        }
+
+        [Test]
+        public void SuccessfulLogin()
+        {
+            //1. open webpage
+            Browser.driver.Navigate().GoToUrl("https://fakestore.testelka.pl/moje-konto/");
+            //2. insert user and password and click login button
+
+            //3. check username after login
+
+            Assert.Pass();
+        }
+
+        [Test]
+        public void FailedLogin()
+        {
+            //1. open webpage
+            Browser.driver.Navigate().GoToUrl("https://fakestore.testelka.pl/moje-konto/");
+            //2. insert wrong user and password and click login button
+
+            //3. check error message
+
+            Assert.Pass();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            Browser.Cleanup();
+        }
+    }
+}
