6f5cc416fba3b516652bc568781f29939745226a
Tomasz Statkowski <Tomasz.Statkowski@chrobinson.com>
2021-07-28 16:06:02 +0200

    Add new classes

---
+++ FakeStore.TestCases/FakeStore/TC_Login.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace TestAutomationLibrary.Tests.FakeStore
+{
+    [TestFixture]
+    public class TC_Login
+    {
+        [Test]
+        public void SuccessfulLogin()
+        {
+            //1. open webpage
+
+            //2. insert user and password and click login button
+
+            //3. check username after login
+
+            Assert.True(true);
+        }
+
+        [Test]
+        public void FailedLogin()
+        {
+            //1. open webpage
+
+            //2. insert wrong user and password and click login button
+
+            //3. check error message
+
+            Assert.True(true);
+        }
+    }
+}
