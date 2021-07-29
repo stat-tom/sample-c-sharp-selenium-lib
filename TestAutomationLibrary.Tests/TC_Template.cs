16ee2a6947fe4cd8e425b21b690d2973b43c2995
Tomasz Statkowski <Tomasz.Statkowski@chrobinson.com>
2021-07-29 09:44:46 +0200

    Add TC_Template

---
+++ TestAutomationLibrary.Tests/TC_Template.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using TestAutomationLibrary.Selenium;
+
+namespace TestAutomationLibrary.Tests
+{
+    public class TC_Template
+    {
+        //Arrange
+
+        [SetUp]
+        public void Init()
+        {
+            Browser.Init();
+        }
+        
+
+        [Test]
+        public void TestTemplate()
+        {
+            //Act
+            
+            
+
+            //Assert
+            
+
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            Browser.Cleanup();
+        }
+    }
+}
